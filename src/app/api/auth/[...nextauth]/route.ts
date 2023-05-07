b72696b9ebb7f79ca7c31455f1440dc359ab7343
James Dimonaco <james@whereyouat.co.uk>
2023-05-07 19:21:17 +0100

    setup database prisma too

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,12 @@
 import NextAuth from 'next-auth';
 import type { NextAuthOptions } from 'next-auth';
 import GoogleProvider from "next-auth/providers/google";
+import { prisma } from '@/lib/prisma';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+
 
 export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
         clientId: process.env.GOOGLE_CLIENT_ID!,
